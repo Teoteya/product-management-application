a4b8ea4c50c2646a257315eeb97cde4acd4c850e
Teoteya <53837577+Teoteya@users.noreply.github.com>
2025-05-17 14:23:13 +0300

    Changed ci-cd file

--- product-management-system/src/main/java/com/example/product_management_system/config/WebConfig.java
+++ product-management-system/src/main/java/com/example/product_management_system/config/WebConfig.java
@@ -3,16 +3,34 @@ package com.example.product_management_system.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-
+//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+  /*  @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/views/"); // мешал статическим ресурсам, spring не находил css файлы
+        resolver.setSuffix(".jsp");
+        registry.viewResolver(resolver);
+    }  */
 
     @Bean
     public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
         return new HiddenHttpMethodFilter();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
